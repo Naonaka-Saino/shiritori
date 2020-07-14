bad61e94bebf805e2f696eba09fa590f20210491
nao.saino <saino305@gmail.com>
2020-07-14 16:08:21 +0000

    debug

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/new'
+  get 'users/create'
+  
+  #post 'toppage/index', to: 'toppage#index'  
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'toppages#index'
+  
+  root 'toppages#index'
+  resources :toppage
+  #post 'toppage/index', to: 'toppage#index'
   
 end
