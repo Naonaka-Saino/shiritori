b233ec2d726386248a4d90d71e1d6eecbe97751d
nao.saino <saino305@gmail.com>
2020-07-16 04:56:43 +0000

    api

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,32 +1,27 @@
 class ToppagesController < ApplicationController
+  
   def index
-    require 'open-uri'
+    unless params[:shiritori_code].nil?
+    require 'net/http'
     require 'json'
-    #require 'addressable/uri'
-        
-        # 1.urlを解析する
-    url = URI.parse URI.encode "http://wikipedia.simpleapi.net/api?keyword=野球&output=json"
-    #url = URI.parse("http://wikipedia.simpleapi.net/api?keyword=TCP/IP")
-        # 2.httpの通信を設定する
-        # 通信先のホストやポートを設定
-        results = JSON.parse(open(url).read, {symbolize_names: true})
-        @title=[]
-        results.each do |res|
-        title= res["title"].to_s
-        @title.push(title)
-        end
-        puts @title
-        #[0]["title"]
-        #https = Net::HTTP.new(url.host, url.inferred_port)
-        # httpsで通信する場合、use_sslをtrueにする
-        #https.use_ssl = true
-        # 3.リクエストを作成する
-        #req = Net::HTTP::Get.new(url.path)
-        # 4.リクエストを投げる/レスポンスを受け取る
-        #res = https.request(req)
-        # 5.データを変換する
-        #hash = JSON.parse(res.body)
-        # 結果を出力
-        #puts hash["results"][0]["title"]
+    require 'uri'
+    
+    word=params[:shiritori_code]
+    uri = URI.encode "http://wikipedia.simpleapi.net/api?keyword=#{word}&output=json"
+    
+    uri = URI.parse(uri)
+    req = Net::HTTP::Get.new(uri)
+    res = Net::HTTP.start(uri.host, uri.port){|http|
+    http.request(req)
+    }
+    
+    results = JSON.parse(res.body)
+    @title=[]
+    results.each do |res|
+    title= res["title"].to_s
+    @title.push(title)
+    end
+    puts @title
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/new'
-  get 'users/create'
-  
-  #post 'toppage/index', to: 'toppage#index'  
-  
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
   root 'toppages#index'
-  resources :toppage
-  #post 'toppage/index', to: 'toppage#index'
+  resources :users
+  resources :toppages
   
 end
