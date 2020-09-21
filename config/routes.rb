dacd5ca38f87fb8a0cef1e4a9f8314ba1dcf1e4c
nao.saino <saino305@gmail.com>
2020-09-21 05:49:53 +0000

    restart

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   root 'toppages#index'
-  resources :users
+  resources :users, only: [:index, :show, :new, :create]
   resources :toppages
   
 end
